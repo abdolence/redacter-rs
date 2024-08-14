b8d6e2f632cb28b9cdda91a74e8ea9c62b3729a8
Abdulla Abdurakhmanov <me@abdolence.dev>
2024-08-14 12:38:01 +0200

    Make PDF render feature flag to disable

--- src/file_converters/mod.rs
+++ src/file_converters/mod.rs
@@ -3,6 +3,9 @@ use crate::AppResult;
 
 pub mod pdf;
 
+#[cfg(feature = "pdf-render")]
+mod pdf_image_converter;
+
 pub struct FileConverters {
     pub pdf_image_converter: Option<Box<dyn PdfToImage + 'static>>,
 }
@@ -14,13 +17,19 @@ impl FileConverters {
         }
     }
 
+    #[cfg(feature = "pdf-render")]
     pub async fn init(&mut self) -> AppResult<()> {
-        match pdf::PdfImageConverter::new().ok() {
+        match crate::file_converters::pdf_image_converter::PdfImageConverter::new().ok() {
             Some(pdf_image_converter) => {
                 self.pdf_image_converter = Some(Box::new(pdf_image_converter));
                 Ok(())
             }
             None => Ok(()),
         }
+    }
+
+    #[cfg(not(feature = "pdf-render"))]
+    pub async fn init(&mut self) -> AppResult<()> {
+        Ok(())
     }
 }
